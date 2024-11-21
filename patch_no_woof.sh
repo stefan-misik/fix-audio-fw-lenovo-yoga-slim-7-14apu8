6417782c72438e3aee8813e28e90ee7e2de7b4bf
Stefan Misik <stefan-misik@users.noreply.github.com>
2024-11-21 10:35:10 +0100

    Add script

---
+++ patch_no_woof.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+
+file=TIAS2781RCA4.bin.zst
+
+backup="$file.backup"
+
+
+# Check if backup file exists
+[ -e "$backup" ] && ( echo "Old backup still exists, exiting..." ; exit 1 )
+
+
+tmpin="$(mktemp -q)"
+tmpout="$(mktemp -q)"
+
+zstd -q -f -d "$file" -o "$tmpin"
+hexdump -v -e '1/1 "%02X "' "$tmpin" | sed 's/00 00 5C D9/00 00 5C 19/g' | xxd -r -p - "$tmpout"
+cp "$file" "$backup"
+zstd -q -f -z "$tmpout" -o "$file"
+rm "$tmpin" "$tmpout"
