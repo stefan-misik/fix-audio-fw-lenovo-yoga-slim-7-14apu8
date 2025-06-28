acc8766323d552b23a718dc5da10b38214989f0a
Stefan Misik <stefan-misik@users.noreply.github.com>
2025-06-28 11:24:10 +0200

    Modify firmware file directly

--- patch_no_woof.sh
+++ patch_no_woof.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 
-file=TIAS2781RCA4.bin.zst
+file=/usr/lib/firmware/ti/tas2781/TIAS2781RCA4.bin.zst
 
 backup="$file.backup"
 
 
 # Check if backup file exists
-[ -e "$backup" ] && ( echo "Old backup still exists, exiting..." ; exit 1 )
+[ -e "$backup" -a "$1" != "-f" ] && ( echo "Old backup still exists, exiting..." ; exit 1 )
 
 
 tmpin="$(mktemp -q)"
@@ -18,3 +18,4 @@ hexdump -v -e '1/1 "%02X "' "$tmpin" | sed 's/00 00 5C D9/00 00 5C 19/g' | xxd -
 cp "$file" "$backup"
 zstd -q -f -z "$tmpout" -o "$file"
 rm "$tmpin" "$tmpout"
+
